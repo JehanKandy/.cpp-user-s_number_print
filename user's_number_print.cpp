8c4e2c9def4730392f0d36755279355456a05f0f
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-11-15 20:52:24 -0500

    Add files via upload

---
+++ user's_number_print.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "Enter Any Number : ";
+    cin >> num;
+
+    cout << " \n You Entered : " << num << endl;
+
+
+    return 0;
+}
